9da5ced464b624157a7ffb1650c8a143d9f33f13
muhfarrasm <muhammadfarrasm@gmail.com>
2023-05-17 19:17:53 +0700

    Membuat prosedur mahasiswa::printAll

--- mbrstatic/mbrstatic.cpp
+++ mbrstatic/mbrstatic.cpp
@@ -15,6 +15,16 @@ public:
 
 int mahasiswa::nim = 0;
 
-void mahasiswa::setID() {
+void mahasiswa::setID() 
+{
 	id = ++nim;
 }
+
+void mahasiswa::printAll() 
+{
+	cout << "ID   = " << id << endl;
+	cout << "Nama = " << nama << endl;
+	cout << "NIM  = " << nim << endl;
+	cout << endl;
+}
+

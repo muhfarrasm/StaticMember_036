86c26f7e99ce9e6877f826a0fc810b3c1c22289e
muhfarrasm <muhammadfarrasm@gmail.com>
2023-05-17 19:34:02 +0700

    program utama

--- chainfunction/chainfunction.cpp
+++ chainfunction/chainfunction.cpp
@@ -18,3 +18,10 @@ public:
 	}
 }bukunya;
 
+int main()
+{
+	bukunya.setJudul("Matematika");
+	cout << bukunya.getJudul();
+	cout << bukunya.setJudul("Matematika").getJudul(); //chain function calls
+	return 0;
+}
